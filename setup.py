2adda1009e7e1d8e7cd084397fc762b2a12b37b8
Marius van Niekerk <marius.v.niekerk@gmail.com>
2017-01-09 12:46:10 -0500

    Generate the rst on demand

--- setup.py
+++ setup.py
@@ -2,15 +2,20 @@ from setuptools import setup, find_packages
 import versioneer
 
 try:
-    import pypandoc
-    long_description = pypandoc.convert('README.md', 'rst')
-except ImportError:
-    long_description = open('README.md').read()
+    try:
+        import pypandoc
+        long_description = pypandoc.convert('README.md', 'rst')
+    except ImportError:
+        long_description = open('README.md').read()
+    with open("README.rst", 'w') as fo:
+        fo.write(long_description)
+except:
+    pass
 
 setup(
     name='spylon-kernel',
     description='Jupyter metakernel for apache spark and scala',
-    long_description=long_description,
+    long_description=open('README.rst').read(),
     version=versioneer.get_version(),
     cmdclass=versioneer.get_cmdclass(),
     url='http://github.com/mariusvniekerk/spylon-kernel',
