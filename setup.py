538f20174dc72adf7f462a73dea0b952b85f1510
Marius van Niekerk <marius.v.niekerk@gmail.com>
2017-01-09 14:40:29 -0500

    Fixed setup.py

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
     version=versioneer.get_version(),
     cmdclass=versioneer.get_cmdclass(),
     url='http://github.com/mariusvniekerk/spylon-kernel',
-    requirements=['spylon[spark]', 'metakernel', 'jedi', 'tornado'],
+    install_requires=['spylon[spark]', 'metakernel', 'jedi', 'tornado', 'ipykernel'],
     packages=list(find_packages()),
     author='Marius van Niekerk',
     author_email='marius.v.niekerk+spylon@gmail.com',
