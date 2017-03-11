213be830774eccbfbe5ad4fc46811a549b6802d3
Peter Parente <parente@cs.unc.edu>
2017-03-10 23:11:16 -0500

    Automate local dev setup

--- run_tests.py
+++ run_tests.py
@@ -3,13 +3,6 @@ import sys
 import pytest
 
 if __name__ == '__main__':
-    # show output results from every test function
-    args = ['-vv']
-    # show the message output for skipped and expected failure tests
-    args.append('-rxs')
-    args.append('--capture=sys')
-    args.append('--color=yes')
-    args.extend(sys.argv[1:])
     # call pytest and exit with the return code from pytest so that
     # travis will fail correctly if tests fail
-    sys.exit(pytest.main(args))
+    sys.exit(pytest.main(sys.argv[1:]))
